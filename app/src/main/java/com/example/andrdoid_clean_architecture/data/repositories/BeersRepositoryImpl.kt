74093cd91c7a2f25e1cf122fd824fe59493ba385
James <james.jasenia@gmail.com>
2022-08-13 14:35:19 +1000

    Created concrete BeersRepositoryImpl.

--- app/src/main/java/com/example/andrdoid_clean_architecture/data/repositories/BeersRepositoryImpl.kt
+++ app/src/main/java/com/example/andrdoid_clean_architecture/data/repositories/BeersRepositoryImpl.kt
@@ -1,4 +1,16 @@
 package com.example.andrdoid_clean_architecture.data.repositories
 
-class BeersRepositoryImpl {
+import com.example.andrdoid_clean_architecture.data.remote.PunkApi
+import com.example.andrdoid_clean_architecture.data.remote.dto.toBeer
+import com.example.andrdoid_clean_architecture.domain.model.Beer
+import com.example.andrdoid_clean_architecture.domain.repositories.BeersRepository
+import javax.inject.Inject
+
+class BeersRepositoryImpl @Inject constructor(
+    private val api: PunkApi
+) : BeersRepository {
+    override suspend fun getBeers(): List<Beer> {
+        val beers = api.getBeers()
+        return beers.map { it.toBeer() }
+    }
 }
