d9db0788e6059f3f580991a1f9534353755b69e7
James <james.jasenia@gmail.com>
2022-08-13 14:21:38 +1000

    Created BeersRepository interface in the domain module.

--- app/src/main/java/com/example/andrdoid_clean_architecture/domain/repositories/BeersRepository.kt
+++ app/src/main/java/com/example/andrdoid_clean_architecture/domain/repositories/BeersRepository.kt
@@ -1,4 +1,9 @@
 package com.example.andrdoid_clean_architecture.domain.repositories
 
+import com.example.andrdoid_clean_architecture.domain.model.Beer
+
 interface BeersRepository {
+
+    suspend fun getBeers(): List<Beer>
+
 }
