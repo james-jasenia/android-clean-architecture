14248d2b584fc29ca040a7e8b97b80be9a24b613
James <james.jasenia@gmail.com>
2022-08-13 15:05:11 +1000

    Created GetBeersUseCase and implemented GetBeers method.

--- app/src/main/java/com/example/andrdoid_clean_architecture/domain/use_cases/GetBeersUseCase.kt
+++ app/src/main/java/com/example/andrdoid_clean_architecture/domain/use_cases/GetBeersUseCase.kt
@@ -1,4 +1,24 @@
 package com.example.andrdoid_clean_architecture.domain.use_cases
 
-class GetBeersUseCase {
+import com.example.andrdoid_clean_architecture.domain.model.Beer
+import com.example.andrdoid_clean_architecture.domain.repositories.BeersRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetBeersUseCase @Inject constructor(
+    private val repository: BeersRepository
+) {
+    operator fun invoke() : Flow<Result<List<Beer>>> = flow {
+        try {
+            val beers = repository.getBeers()
+            emit(Result.success(beers))
+        } catch(error: IOException) {
+            emit(Result.failure(error))
+        } catch(error: HttpException) {
+            emit(Result.failure(error))
+        }
+    }
 }
