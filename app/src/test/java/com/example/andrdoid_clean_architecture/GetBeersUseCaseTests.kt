29a023584910062e85ef0477fcbbd20ee5289211
James <james.jasenia@gmail.com>
2022-08-13 19:07:00 +1000

    Test: `test_getBeers_returnError_onError`.

--- app/src/test/java/com/example/andrdoid_clean_architecture/GetBeersUseCaseTests.kt
+++ app/src/test/java/com/example/andrdoid_clean_architecture/GetBeersUseCaseTests.kt
@@ -6,9 +6,15 @@ import com.example.andrdoid_clean_architecture.domain.use_cases.GetBeersUseCase
 import junit.framework.Assert.assertTrue
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
+import retrofit2.HttpException
+import retrofit2.Response
+import kotlin.test.assertFailsWith
+
 
 class GetBeersUseCaseTests {
 
@@ -23,5 +29,24 @@ class GetBeersUseCaseTests {
         val receivedResult = sut.invoke().first().getOrNull()
 
         assertTrue(expectedResult == receivedResult)
+    }
+
+    @Test
+    fun test_getBeers_returnError_onError() = runBlocking {
+        val errorResponse = """{
+            "type": "error",
+            "message": "anyMessage"
+        }"""
+        val errorResponseBody = errorResponse.toResponseBody("application/json".toMediaTypeOrNull())
+        val expectedResult = HttpException(Response.error<String>(400, errorResponseBody))
+        val mockRepository = mock<BeersRepository>()
+        whenever(mockRepository.getBeers()).then {
+            throw expectedResult
+        }
+
+        val sut = GetBeersUseCase(mockRepository)
+        var receivedError = sut.invoke().first().exceptionOrNull()
+
+        assertTrue(expectedResult == receivedError)
     }
 }
