fec5d68f6980ff71bda26e3392df0c29e63aa434
James <james.jasenia@gmail.com>
2022-08-14 21:03:10 +1000

    Bug Fix: Changed `first` to `firstOrNull` to prevent potential crash when an empty list is returned.

--- app/src/main/java/com/example/andrdoid_clean_architecture/presentation/get_beers/BeersListActivity.kt
+++ app/src/main/java/com/example/andrdoid_clean_architecture/presentation/get_beers/BeersListActivity.kt
@@ -2,27 +2,31 @@ package com.example.andrdoid_clean_architecture.presentation.get_beers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.example.andrdoid_clean_architecture.databinding.ActivityBeersListBinding
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
-import javax.inject.Inject
 
+@AndroidEntryPoint
 class BeersListActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivityBeersListBinding
+    private val viewModel: GetBeersViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBeersListBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        setupBindings()
+        setupBindings()
     }
 
-//    private fun setupBindings() {
-//        lifecycleScope.launchWhenCreated {
-//            viewModel.stateFlow.collectLatest {
-//                binding.beerTextView.text = it.first().name
-//            }
-//        }
-//    }
+    private fun setupBindings() {
+        lifecycleScope.launchWhenCreated {
+            viewModel.stateFlow.collectLatest {
+                binding.beerTextView.text = it.firstOrNull()?.name
+            }
+        }
+    }
 }
